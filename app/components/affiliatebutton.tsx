bbbfeb194eb23855f63198488a23ba35757ea5ea
Craig Ronald <55587121+CraigRonald555@users.noreply.github.com>
2024-12-01 15:34:27 +0000

    Adjust width of the affiliate button on smaller screens to fill with screen

--- app/components/affiliatebutton.tsx
+++ app/components/affiliatebutton.tsx
@@ -9,7 +9,7 @@ type AffiliateButtonProps = {
 const AffiliateButton: React.FC<AffiliateButtonProps> = ({ image, alt, text }) => {
 
     return (
-        <div className="flex flex-col justify-around items-center p-[12px] border border-[#CBD5D9] rounded-[8px] h-[150px] w-[50%] cursor-pointer">
+        <div className="flex flex-col justify-around items-center p-[12px] border border-[#CBD5D9] rounded-[8px] h-[150px] w-full md:w-[50%] cursor-pointer">
             <Image src={image} alt={alt} width={100} height={100} />
             <p className="text-center text-accent-dark text-[14px]">{text}</p>
         </div>
